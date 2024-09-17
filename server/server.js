abc05b884dd473409e1bbeff82450295fc4457da
Cameron Hirsh <chirsh1@uncc.edu>
2024-09-17 17:07:40 -0400

    V1

--- server/server.js
+++ server/server.js
@@ -1,23 +1,12 @@
-const express = require("express");
-const userRouter = require("./routes/user-routes");
-const blogRouter = require("./routes/blog-routes");
-require("./config/db");
-const cors = require("cors");
-
+const express = require('express');
 const app = express();
 
-app.use(cors());
-
-app.set("view engine", "ejs");
-app.use(express.json());
-
-app.use("/api/users", userRouter);
-app.use("/api/blogs", blogRouter);
-
-app.use("/api", (req, res, next) => {
-  res.send("hello");
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-//define port
-
-app.listen(5001, () => console.log("app started at 5001..."));
+// Start the server
+app.listen(5001, () => {
+  console.log('Server started on port 5001');
+});
